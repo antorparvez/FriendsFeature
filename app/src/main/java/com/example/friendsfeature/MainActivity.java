84c557b3dd8614e95b7cc2ad62f959be782afa5c
Foqrul <foqrulislam426@gmail.com>
2020-10-04 00:20:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/friendsfeature/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.friendsfeature;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
